380ea4010b7283c691d8ded4fc3996e257eb4d08
maexx393 <manx393@gmail.com>
2019-06-24 15:39:10 +0200

    Add files via upload

--- src/containers/WalletPage/WidthDraw.js
+++ src/containers/WalletPage/WidthDraw.js
@@ -11,86 +11,23 @@ import { handleChangeWithdraw, fetchSubmitWithdraw } from '../../actions/withdra
 
 import imgVip from '../../assets/images/vip.svg';
 import CoinList from './List';
-import History from './History';
 class WithDraw extends Component {
-  constructor(props){
-    super(props);
-    this.state={
-      ShowTab: 1,
-      name:'',
-      type:'coin',
-      id:''
-    }}
-  componentDidMount() {
-    const {CionDetails}=this.props
-  const   {base_factor,
-    deposit_fee,
-    id,
-    min_deposit_amount,
-    min_withdraw_amount,
-    name,
-    precision,
-    symbol,
-    type,
-    withdraw_fee,
-    withdraw_limit_24h,
-    withdraw_limit_72h,
-   } = CionDetails
-this.setState({
-  name:name,id:id,type:type
-})
-if(type === 'coin'){
-  this.setState({ShowTab:1});
-}else{
-  this.setState({ShowTab:2});
-}
+  componentDidMount() { }
 
-   }
-   componentWillReceiveProps(newProps) {
-    const {CionDetails}=this.props
-  const   {base_factor,
-    deposit_fee,
-    id,
-    min_deposit_amount,
-    min_withdraw_amount,
-    name,
-    precision,
-    symbol,
-    type,
-    withdraw_fee,
-    withdraw_limit_24h,
-    withdraw_limit_72h,
-   } = CionDetails
-this.setState({
-  name:name,id:id,type:type
-})
-if(type === 'coin'){
-  this.setState({ShowTab:1});
-}else{
-  this.setState({ShowTab:2});
-}
-
-   }
   //FIXME: query the correct history
   // filterWithDraw = list => list.filter(item => item.currency === this.props.activeWallet);
-  handleTabe=(tab)=>{
-    this.setState({ShowTab:tab});
-  }
-  render() {
-    const {ShowTab,name,id} = this.state
 
+  render() {
     return (
       <div className="tab-pane  fade show active" id="v-pills-zilliqua-btc-withdrawl" role="tabpanel"
         aria-labelledby="v-pills-zilliqua-btc-withdrawl-tab">
-                    {ShowTab == 1&&<div>
-                      
-        <h4 className="crypt-down">Withdraw {id.toUpperCase()}</h4>
+        <h4 className="crypt-down">Wire bank transfer</h4>
         <p><i className="pe-7s-info"></i> Standard bank transfer will be made up to 2 workdays</p>
         <form>
           <div className="input-group mb-3">
             <input type="text" placeholder="Amount" className="form-control" name="amount" />
             <div className="input-group-append">
-              <span className="input-group-text crypt-up">{id.toUpperCase()}</span>
+              <span className="input-group-text">USD</span>
             </div>
           </div>
           <div className="input-group mb-3">
@@ -109,7 +46,7 @@ if(type === 'coin'){
             <input type="text" className="form-control" placeholder="Name" name="name" />
           </div>
           <div className="form-group">
-            <input type="text" className="form-control" placeholder="Bank Name" name="swift" />
+            <input type="text" className="form-control" placeholder="Swift Code" name="swift" />
           </div>
           <div className="form-group">
             <div className="form-group">
@@ -131,57 +68,8 @@ if(type === 'coin'){
               </label>
             </div>
           </div>
-          <ul  className="crypt-heading-menu fright" >
-            <li  className="crypt-box-menu menu-red">
-
-          <a href="#withdraw"  className="crypt-button-red-full"> Withdraw</a>
-            </li>
-          </ul>
+          <a href="#withdraw" className="crypt-button-red-full">Initiate Withdraw</a>
         </form>
-        <History />
-        
-        </div>}
-        {ShowTab == 2&&<div>
-                      
-                      <h4 className="crypt-down">Send {name}</h4>
-                      <p><i className="pe-7s-info"></i> Standard bank transfer will be workdays</p>
-                      <form>
-                        <div className="input-group mb-3">
-                          <input type="text" placeholder="Amount" className="form-control" name="amount" />
-                          <div className="input-group-append">
-                            <span className="input-group-text crypt-up">Withdraw All</span>
-                          </div>
-                        </div>
-                        <div className="input-group mb-3">
-                          <input type="text" placeholder="Recipient Address" className="form-control" name="bank-account" />
-                          <div className="input-group-append">
-                            <button className="btn btn-outline-secondary dropdown-toggle" type="button"
-                              data-toggle="dropdown">Other Address</button>
-                            <div className="dropdown-menu">
-                              <a className="dropdown-item" href="#/do-something">234235234</a>
-                              <a className="dropdown-item" href="#/do-something">2343453453</a>
-                              <a className="dropdown-item" href="#/do-something">234234234234</a>
-                            </div>
-                          </div>
-                        </div>
-                        <div className="form-group">
-                          <div className="form-check">
-                            <input className="form-check-input" type="checkbox" id="recipient-btc" />
-                            <label className="form-check-label" htmlFor="recipient-btc">
-                              Add To recipient
-                            </label>
-                          </div>
-                        </div>
-                        <ul  className="crypt-heading-menu fright" >
-            <li  className="crypt-box-menu menu-red">
-
-          <a href="#withdraw"  className="crypt-button-red-full"> Send</a>
-            </li>
-          </ul>
-          <History />
-                      </form>
-                      
-                      </div>}
       </div>);
   }
 }
