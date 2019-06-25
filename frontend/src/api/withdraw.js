e45f29759fbdd0549a53c851e317c9ff4c9b6698
MaoPeng94 <50186753+MaoPeng94@users.noreply.github.com>
2019-06-24 19:21:47 -0700

    delete all

--- frontend/src/api/withdraw.js
+++
@@ -1,7 +0,0 @@
-import axiosInstance from './requestBuilder';
-
-
-export const postNewWithdraws = data => {
-  return axiosInstance.post('/api/v1/applogic/withdraws', data)
-    .then(response => response.data);
-};
